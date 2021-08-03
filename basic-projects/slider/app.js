e2804dfa54f6e89ee71dcbeeeaadb4378878252c
Zaur Sharifov <code.zaursharifov@hotmail.com>
2021-08-03 17:07:46 +0400

    Deleted incomplete files

--- basic-projects/slider/app.js
+++
@@ -1,7 +0,0 @@
-const imgItem = document.getElementById('img-item');
-const leftBtn = document.getElementById('left');
-const rightBtn = document.getElementById('right');
-
-for( var i=0; i<10; i++){
-    console.log(imgItem[i]);
-}
--- nodejs/app.js
+++
@@ -1,6 +0,0 @@
-let sayi = 5;
-if(sayi === 5) {
-    console.log("sayi 5`e esit");
-}else{
-    console.log("sayi 5 DEGIl");
-}
